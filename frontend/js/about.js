af82e26baede28fb3a1495f75a8f29059cb6713f
Hecate <hecate946@gmail.com>
2023-06-23 04:08:17 -0700

    implement javascript typewriter feature

--- frontend/js/about.js
+++ frontend/js/about.js
@@ -1,21 +1,49 @@
-var text = $('.typewriter').text();
+// constant to set the wait between character prints.
+const DELAY_IN_MILLISECONDS = 20;
 
-var length = text.length;
-var timeOut;
-var character = 0;
+// a typewriter is any html element that has the .typewriter class.
+// every typewriter will be printed out dynamically character by character.
+var typewriters = document.querySelectorAll(".typewriter");
+window.addEventListener("load", () => {
+    // first typewriter element.
+    elem = typewriters[0]
+    // we have a typewriter.
+    if (elem)
+    {   // make the initial typewriter call with the first element.
+        typeWriter(typewriters[0].innerHTML, 0, 0);
+    }
+});
 
-
-(function typeWriter() { 
-    timeOut = setTimeout(function() {
-        $('.typewriter').css("opacity", 1);
-        character++;
-        var type = text.substring(0, character);
-        $('.typewriter').text(type);
-        typeWriter();
-        
-        if (character == length) {
-            clearTimeout(timeOut);
-        }
-        
-    }, 7);
-}());
+var time; // create a storage variable for the timeout.
+function typeWriter(initial_text, index, char) {
+    // fetch the element we're currently working with.
+    elem = typewriters[index];
+    // only do work if it exists.
+    if (elem)
+    {
+        // use setTimeout to create a delay.
+        time = setTimeout(function() {
+            // create a string with the current characters to display
+            var type = initial_text.substring(0, char);
+            // set the opacity to 1 so we can see the text.
+            elem.style.opacity = 1;
+            // update the html to contain the new text.s
+            elem.innerHTML = type;
+            // recursively call the typewriter to print the next character.
+            typeWriter(initial_text, index, char + 1);
+            
+            // we've reached the end of a typewriter.
+            if (char == initial_text.length) {
+                // end the timeout session
+                clearTimeout(time);
+                // check if there is another typewriter after.
+                var next_elem = typewriters[index + 1];
+                // if we have a new typewriter.
+                if (next_elem) {
+                    // repeat the process with this new typewriter.
+                    typeWriter(next_elem.innerHTML, index + 1, 0);
+                }
+            }
+        }, 10);
+    }
+}
