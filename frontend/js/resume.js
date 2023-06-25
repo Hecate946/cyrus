7c8afe7936fc55357bd96c429efdbfa259ef8835
Hecate <hecate946@gmail.com>
2023-06-25 00:03:09 -0700

    make more updates to the resume

--- frontend/js/resume.js
+++ frontend/js/resume.js
@@ -22,6 +22,5 @@ var skillBars = document.querySelectorAll(".skills-bar");
 skillBars.forEach(function(skillBar) {
     var progressBar = skillBar.firstElementChild;
     var percentage = skillBar.parentElement.getAttribute("data-percent");
-    var width = (percentage/100) * skillBar.offsetWidth;
-    progressBar.style.maxWidth = width;
+    progressBar.style.maxWidth = percentage + "%";
 })
